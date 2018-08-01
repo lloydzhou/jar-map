303cfeb5d3390498102a3cd7a85f3a849f265578
lloydzhou <lloydzhou@qq.com>
2018-08-01 11:09:02 +0800

    update

--- dva/src/components/Example.js
+++ dva/src/components/Example.js
@@ -1,28 +1,8 @@
 import React from 'react';
-import { Map, GroundImage } from 'react-amap';
+import { connect } from 'dva';
+import { Map, GroundImage, Polyline } from 'react-amap';
 import tower from '../assets/tower.png';
 
-const T = 0.012;
-function randomBoundsAndCenter(center) {
-  // 随机生成一个中心点，然后据此生成一个边界
-  // 30.894930, 114.025867
-  center = center || {
-    longitude: 114 + Math.random() * T,
-    latitude: 30 + Math.random() * T,
-  }
-  const bounds = {
-    sw: {
-      longitude: center.longitude - T,
-      latitude: center.latitude - T,
-    },
-    ne: {
-      longitude: center.longitude + T,
-      latitude: center.latitude + T,
-    }
-  };
-  console.log(center, bounds)
-  return { center, bounds };
-}
 class MapDemo extends React.Component{
   constructor(){
     super();
@@ -31,56 +11,53 @@ class MapDemo extends React.Component{
       click: () => {console.log('img click')},
       dblclick: () => {console.log('img dblclick')},
     };
-    const bc = randomBoundsAndCenter({
-      // 30.894930, 114.025867
-      longitude: 114.025867,
-      latitude: 30.907930,
-    })
-    const bc1 = randomBoundsAndCenter({
-      longitude: 114.0825867,
-      latitude: 30.908930,
-    })
-    const bc2 = randomBoundsAndCenter({
-      longitude: 114.1225867,
-      latitude: 30.909930,
-    })
     this.state = {
       src: tower,
       visible: true,
       opacity: 1,
-      bounds: bc.bounds,
-      bounds1: bc1.bounds,
-      bounds2: bc2.bounds,
-      mapCenter: bc1.center,
     };
   }
   render(){
+    const { lines, lineIndex, dispatch } = this.props;
+    const T = 0.0009;
     return <div>
       <div style={{width: '100%', height: '500px'}}>
-        <Map plugins={['ToolBar']} center={this.state.mapCenter} zoom={12}>
-          <GroundImage
-            visible={this.state.visible}
-            events={this.events}
-            bounds={this.state.bounds1}
-            src={this.state.src}
-            opacity={this.state.opacity}
-            clickable
-          />
-          <GroundImage
-            visible={this.state.visible}
-            events={this.events}
-            bounds={this.state.bounds2}
-            src={this.state.src}
-            opacity={this.state.opacity}
-            clickable
-          />
-          <GroundImage
-            visible={this.state.visible}
-            events={this.events}
-            bounds={this.state.bounds}
-            src={this.state.src}
-            opacity={this.state.opacity}
-            clickable
+        <Map
+          plugins={['ToolBar', 'MapType', 'Scale']}
+          center={lines[lineIndex].data[Math.floor(lines[lineIndex].data.length / 2)]}
+          zoom={16}
+        >
+          {lines[lineIndex].data.map(center => {
+            const bounds = {
+              sw: {
+                longitude: center.longitude - T,
+                latitude: center.latitude - T,
+              },
+              ne: {
+                longitude: center.longitude + T,
+                latitude: center.latitude + T,
+              }
+            };
+            console.log('bounds', bounds, center)
+            return (
+              <GroundImage
+                key={center.no}
+                visible={this.state.visible}
+                events={this.events}
+                opacity={this.state.opacity}
+                bounds={bounds}
+                src={tower}
+                clickable
+              />
+            )
+          })}
+          <Polyline 
+            path={lines[lineIndex].data.map(t => {
+              return {
+                longitude: t.longitude,
+                latitude: t.latitude + T * 0.8,
+              }
+            })}
           />
         </Map>
       </div>
@@ -88,17 +65,7 @@ class MapDemo extends React.Component{
   }
 }
 
-export default MapDemo;
+export default connect(({example}) => {
+  return example
+})(MapDemo);
 
-//const Example = () => {
-//  return (
-//    <div>
-//      Example
-//    </div>
-//  );
-//};
-//
-//Example.propTypes = {
-//};
-//
-//export default Example;
--- dva/src/routes/IndexPage.js
+++ dva/src/routes/IndexPage.js
@@ -1,18 +1,64 @@
 import React from 'react';
 import { connect } from 'dva';
 import styles from './IndexPage.css';
+import { Layout, Menu, Icon, Breadcrumb } from 'antd';
 import Example from '../components/Example'
+import 'antd/dist/antd.css';
 
-function IndexPage() {
+const { Header, Content, Footer, Sider } = Layout;
+
+
+function IndexPage({lines, lineIndex, dispatch}) {
+  console.log(lines, lineIndex)
   return (
-    <div className={styles.normal}>
-      <Example />
-    </div>
+    <Layout>
+      <Sider
+        breakpoint="lg"
+        collapsedWidth="0"
+        onBreakpoint={(broken) => { console.log(broken); }}
+        onCollapse={(collapsed, type) => { console.log(collapsed, type); }}
+      >
+        <div className="logo" />
+        <Menu
+          theme="dark"
+          mode="inline"
+          defaultSelectedKeys={['' + lineIndex]}
+          onSelect={({item, key, seleckedKey}) => {
+            dispatch({ type: 'example/save', payload: { lineIndex: parseInt(key) } })
+            console.log(item, key, seleckedKey)
+          }}>
+          {lines.map((line, index) => {
+            return <Menu.Item key={index}>
+              <Icon type="line-chart" />
+              <span className="nav-text">{line.name}</span>
+            </Menu.Item>
+          })}
+        </Menu>
+      </Sider>
+      <Layout>
+        <Header style={{ background: '#fff', padding: 0 }}>
+          <Breadcrumb style={{ margin: '16px 24px' }}>
+            <Breadcrumb.Item>Home</Breadcrumb.Item>
+            <Breadcrumb.Item>{lines[lineIndex].name}</Breadcrumb.Item>
+          </Breadcrumb>
+        </Header>
+        <Content style={{ margin: '24px 16px 0' }}>
+          <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>
+            <Example />
+          </div>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>
+          Ant Design ©2018 Created by lloydzhou@qq.com
+        </Footer>
+      </Layout>
+    </Layout>
   );
 }
 
 IndexPage.propTypes = {
 };
 
-export default connect()(IndexPage);
+export default connect(({example}) => {
+  return example
+})(IndexPage);
 
